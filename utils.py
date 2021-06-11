6dca8904a4220a61a7c483473af7d174007be8df
duartejr <duarte.jr105@gmail.com>
2021-06-11 10:17:51 -0300

    add corps engineers2

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ color_palette = {'Ventura': "#32964d", 'CHPW':"#83d996", 'Temez':  "#016876",
                  'Kirpich': "#aedbf0", 'Ven te Chow': "#3444bc",
                  'Pasini': "#f09bf1", 'Picking': "#9525ba",
                  'Pickering': "#528efb", 'Bransby Willians': "#514e72",
-                 'Giandotti': "#1ceaf9", 'Epsey': "#0b5313"
+                 'Giandotti': "#1ceaf9", 'Epsey': "#0b5313",
                  'Corps Engineers': "#0b4343"}
 
 
